f6fed49eeebb9f244cd0257b45c52b4ff23819f2
deepan.ramalingam <deepan.ramalingam@stackroute.in>
2020-04-07 22:03:00 +0530

    initial commit

---
+++ src/test/java/com/stackroute/AppTest.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		Assertions.assertEquals(true, true);
+	}
+
+}
